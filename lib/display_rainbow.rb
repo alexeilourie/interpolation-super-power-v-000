d7da4ba38ad6aac00c21991af5b0e639664ecdf2
Alexei Lourie <alexeilourie@gmail.com>
2018-08-21 18:19:49 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,7 +4,7 @@ def display_rainbow(arr)
   arr.each do |color|
     arr1 << color[0].upcase
   end
-  str = "#{arr1[0]}: #{arr[0]}, #{arr1[1]}: #{arr[1]}, #{arr1[2]}: #{arr[2]}, #{arr1[3]}: #{arr[3]}, #{arr1[4]}: #{arr[4]}, #{arr1[5]}: #{arr[5]}"
+  str = "#{arr1[0]}: #{arr[0]}, #{arr1[1]}: #{arr[1]}, #{arr1[2]}: #{arr[2]}, #{arr1[3]}: #{arr[3]}, #{arr1[4]}: #{arr[4]}, #{arr1[5]}: #{arr[5]}, #{arr1[6]}: #{arr[6]}"
   puts str
   return str
 end
